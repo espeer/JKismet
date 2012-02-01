8ea747596214d62cb929c52b7ad4a746443030fa
Edwin Peer <edwin@peer.co.za>
2012-02-02 01:52:16 +0200

    switch to GPL 2

--- src/za/co/towerman/jkismet/JKismet.java
+++ src/za/co/towerman/jkismet/JKismet.java
@@ -4,7 +4,7 @@
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -47,7 +47,7 @@ public class JKismet {
         };
         
         listener.subscribe(TimeMessage.class, "time");
-        listener.subscribe(BSSIDMessage.class, "mac, channel, frequencies, networkType, dataBytes, carriers, encodings, cryptographies");
+        listener.subscribe(BSSIDMessage.class, "mac, channel, frequencies, networkType, addressType, dataBytes, carriers, encodings, cryptographies");
         
         conn.register(listener);
         
