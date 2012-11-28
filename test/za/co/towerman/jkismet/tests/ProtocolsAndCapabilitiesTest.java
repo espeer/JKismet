98d66bdf051b369a1db1d19c8e801b440b4bc341
Simeon Miteff <simeon.miteff@gmail.com>
2012-11-28 14:07:00 +0200

    Better name for AllCapabilitiesTest (ProtocolsAndCapabilitiesTest), and added GPL header.

--- test/za/co/towerman/jkismet/tests/ProtocolsAndCapabilitiesTest.java
+++ test/za/co/towerman/jkismet/tests/ProtocolsAndCapabilitiesTest.java
@@ -1,6 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * za.co.towerman.jkismet.Protocol
+ * Copyright (C) 2012 Edwin Peer
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package za.co.towerman.jkismet.tests;
 
@@ -29,13 +42,13 @@ import org.junit.Test;
  *
  * @author simeon
  */
-public class AllCapabilitiesTest {
+public class ProtocolsAndCapabilitiesTest {
     
     private KismetConnection conn;
     private KismetListener listener;
     private Map<String, Class<KismetMessage>> pmap;
     
-    public AllCapabilitiesTest() {
+    public ProtocolsAndCapabilitiesTest() {
     }
 
 //    @BeforeClass
